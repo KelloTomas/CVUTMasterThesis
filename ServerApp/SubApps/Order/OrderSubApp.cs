66beb01290e21a00cfca9a8bf80b0e2a2aa2f80b
KelloTomas <kello.tomas@gmail.com>
2018-01-29 20:09:39 +0100

    Implemented Order App

--- ServerApp/SubApps/Order/OrderSubApp.cs
+++ ServerApp/SubApps/Order/OrderSubApp.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Threading;
 using ServerApp.SubApps.Shared.States;
 using ServerApp.Devices;
+using ServerApp.SubApps.Order.Layouts;
+using ServerApp.SubApps.Order.States;
+using ServerApp.Devices.Actions;
+using System.Linq;
+using ServerApp.Data;
 
 namespace ServerApp.SubApps.Order
 {
 	public class OrderSubApp : SubApp, IOrderSubApp
 	{
-		public OrderSubApp(List<Device> devices) : base()
+		public Rallo Rallo { get; private set; }
+		public OrderSubApp(List<Device> devices, DatabaseLayer dbLayer) : base(dbLayer)
 		{
+			if (devices.Count != 1)
+				throw new ArgumentOutOfRangeException();
+			Rallo = new Rallo(this);
+			Rallo.Connect(devices[0].IP, devices[0].Port);
 		}
-
-        public override IStateBase GetInitState()
+		public OrdersLayout OrdersLayout { get; } = new OrdersLayout();
+		public MessageLayout MessageLayout { get; } = new MessageLayout();
+		public override IStateBase GetInitState()
         {
-            return new DefaultState(200);
-        }
-    }
+            return new OrderState(this);
+		}
+		public override IEnumerable<IStoreLayoutAction> GetStoreLayoutActions()
+		{
+			System.Reflection.PropertyInfo[] properties = GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Instance);
+			foreach (System.Reflection.PropertyInfo propertyInfo in properties.Where(p => typeof(Shared.Layouts.LayoutBase).IsAssignableFrom(p.PropertyType)))
+			{
+				Shared.Layouts.LayoutBase layout = (Shared.Layouts.LayoutBase)propertyInfo.GetValue(this, null);
+				yield return new StoreLayoutAction(layout.Name, layout.GetLayout());
+			}
+		}
+	}
 }
---
+++ ServerApp/SubApps/Order/States/OrderState.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Timers;
+using ServerApp.SubApps.Shared.States;
+using ServerApp.Devices;
+using ServerApp.SubApps.Shared.Data;
+using ServerApp.SubApps.Shared.Layouts;
+using ServerApp.Devices.Actions;
+using ServerApp.SubApps.Order.Layouts;
+using System.Data.Entity;
+using ServerApp.Data;
+
+namespace ServerApp.SubApps.Order.States
+{
+	public class OrderState : StateBase
+	{
+
+        #region private fields...
+        List<Menu> menuOnScreen;
+        OrderSubApp app;
+		LayoutBase layout;
+		Client client;
+		DateTime dateToOrder;
+		private string clientMsg = null;
+		IEnumerable<Menu> menu;
+		private int pageNum;
+		private int? selected;
+        bool reset = true;
+		#endregion
+
+		#region constructors...
+		public OrderState(OrderSubApp subApp) : base(3000)
+		{
+			app = subApp;
+		}
+		#endregion
+		public override void Enter()
+        {
+            base.Enter();
+			menu = app.databaseLayer.GetMenu();
+			SetInitValues();
+        }
+
+        private void SetInitValues()
+        {
+            pageNum = 0;
+            selected = null;
+            SetDateToOrder(DateTime.Now.Date);
+            layout = app.OrdersLayout;
+            clientMsg = null;
+        }
+
+        private void SetDateToOrder(DateTime date)
+        {
+            dateToOrder = date;
+            menuOnScreen = menu.Where(m => m.ForDate == dateToOrder).ToList();
+        }
+
+        public override IStateBase ProcessTimerElapsed()
+		{
+			List<IAction> action;
+			switch (layout)
+			{
+				case OrdersLayout ordersLayout:
+					action = new List<IAction>
+					{
+						new ShowLayoutAction(ordersLayout.Name,
+						SetDateTimeToNow()
+						.Concat
+							(ordersLayout.SetDate(dateToOrder, menu.Where(m => m.ForDate > dateToOrder).Any()))
+						.Concat
+							(ordersLayout.SetMeals(menuOnScreen, pageNum, selected.HasValue ? selected.GetValueOrDefault() % app.OrdersLayout.MEALS_PER_PAGE : selected))
+						.ToArray())
+					};
+					break;
+				case MessageLayout messageLayout:
+					action = new List<IAction>
+					{
+						new ShowLayoutAction(messageLayout.Name,
+						SetDateTimeToNow()
+						.Concat
+							(messageLayout.SetText(client, clientMsg))
+						.ToArray()),
+					};
+					break;
+				default:
+					action = new List<IAction>();
+					break;
+			}
+			app.Rallo.SendMessage(new Message(action.ToArray()));
+            if (reset)
+            {
+                SetInitValues();
+            }
+            else
+            {
+                reset = true;
+            }
+			return this;
+		}
+		public override IStateBase ProcessButtonClickAction(ButtonClickAction button, ref bool forceCallStateMethod)
+		{
+            if (button.ButtonName.Contains("menu_"))
+            {
+                selected = int.Parse(button.ButtonName.Last().ToString()) + pageNum * app.OrdersLayout.MEALS_PER_PAGE;
+            }
+            else
+            {
+                switch (button.ButtonName)
+                {
+                    case "prevDay":
+						SetDateToOrder(dateToOrder.AddDays(-1));
+                        break;
+                    case "nextDay":
+						SetDateToOrder(dateToOrder.AddDays(1));
+						break;
+                    case "up":
+                        pageNum--;
+                        break;
+                    case "down":
+                        pageNum++;
+                        break;
+                    default:
+			            return base.ProcessButtonClickAction(button, ref forceCallStateMethod);
+                }
+                selected = null;
+            }
+            reset = false;
+            forceCallStateMethod = true;
+            return this;
+		}
+
+		public override IStateBase ProcessCardReadAction(CardReadAction card, ref bool forceCallStateMethod)
+		{
+			client = app.databaseLayer.GetClient(card.CardNumber);
+				//app.db.Cards.Where(c => c.CardNumber == card.CardNumber).FirstOrDefault()?.Client;
+			layout = app.MessageLayout;
+			if (client == null)
+			{
+				clientMsg = "Neznama karta";
+			}
+			else
+			{
+				if (selected == null)
+					clientMsg = "Nevybrali ste jedlo";
+				else
+				{
+					/*
+					 * ToDo
+					app.db.Orders.Add(new ServerApp.Order() { ForDate = dateToOrder, IdClient = client.IdClient, IdMenu = menuOnScreen[selected.GetValueOrDefault()].IdMenu });
+					app.db.SaveChanges();
+					*/
+					app.databaseLayer.AddOrder(client.Id, dateToOrder, menuOnScreen[selected.GetValueOrDefault()].IdMenu);
+					//throw new NotImplementedException();
+					clientMsg = "Objednane";
+				}
+			}
+			forceCallStateMethod = true;
+			return this;
+		}
+
+		public IEnumerable<ModifyLayoutItem> SetDateTimeToNow()
+		{
+			return SetDateTimeTo(DateTime.Now);
+		}
+		private IEnumerable<ModifyLayoutItem> SetDateTimeTo(DateTime dateTime)
+		{
+			yield return new ModifyLayoutItem("DateValue", "text", dateTime.ToString("d"));
+			yield return new ModifyLayoutItem("TimeValue", "text", dateTime.ToString("t"));
+		}
+	}
+}
